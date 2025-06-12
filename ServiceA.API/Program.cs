2e9331407d1cef3290a258a9b91df10a04cfca0d
Erhan Gündüz <erhangndz@live.com>
2025-06-12 11:08:41 +0300

    Enhance HTTP client and circuit breaker configurations
    Updated the `ProductService` HTTP client to utilize an advanced circuit breaker policy with defined parameters for failure thresholds, sampling duration, minimum throughput, and duration of break. Improved logging for circuit breaker status, including half-open state notifications. Refined the retry policy for better readability and maintainability, aiming to boost the resilience and reliability of HTTP requests in the application.

--- ServiceA.API/Program.cs
+++ ServiceA.API/Program.cs
@@ -1,54 +1,69 @@
 using System.Diagnostics;
 using System.Net;
-using System.Runtime.CompilerServices;
 using Polly;
 using Polly.Extensions.Http;
 using ServiceA.API.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add services to the container.  
 
-
-builder.Services.AddHttpClient<ProductService>(opt =>
+builder.Services.AddHttpClient<ProductService>(client =>
 {
-    opt.BaseAddress = new Uri("https://localhost:5002/api/");
-}).AddPolicyHandler(GetCircuitBreakerPolicy());
+    client.BaseAddress = new Uri("https://localhost:5002/api/");
+}).AddPolicyHandler(GetAdvancedCircuitBreakerPolicy());
 
 
 
 IAsyncPolicy<HttpResponseMessage> GetCircuitBreakerPolicy()
 {
     return HttpPolicyExtensions.HandleTransientHttpError().CircuitBreakerAsync(3, TimeSpan.FromSeconds(10),
-        onBreak: () =>
+        onBreak: (arg1, arg2) =>
         {
-
+            Debug.WriteLine("Circuit Breaker Status => On Break");
         },
         onReset: () =>
         {
-
+            Debug.WriteLine("Circuit Breaker Status => On Reset");
         },
         onHalfOpen: () =>
         {
-
-        } );
+            Debug.WriteLine("Circuit Breaker Status => On Half Open");
+        });
 }
 
-
+IAsyncPolicy<HttpResponseMessage> GetAdvancedCircuitBreakerPolicy()
+{
+    return HttpPolicyExtensions.HandleTransientHttpError().AdvancedCircuitBreakerAsync(
+        failureThreshold: 0.1, // %10 oranýnda baþarýsýz istekleri sayacak 
+        samplingDuration: TimeSpan.FromSeconds(30), //30 saniye boyunca devre çalýþýr durumda
+        minimumThroughput: 3, //30 saniye boyunca 3 istek baþarýsýz olursa devre kesilecek
+        durationOfBreak: TimeSpan.FromSeconds(30), //yeniden açýlmadan önce 30 saniye kapalý kalacak
+        onBreak: (arg1, arg2) =>
+        {
+            Debug.WriteLine("Circuit Breaker Status => On Break");
+        },
+        onReset: () =>
+        {
+            Debug.WriteLine("Circuit Breaker Status => On Reset");
+        },
+        onHalfOpen: () =>
+        {
+            Debug.WriteLine("Circuit Breaker Status => On Half Open");
+        });
+}
 
 IAsyncPolicy<HttpResponseMessage> GetRetryPolicy()
 {
-    return HttpPolicyExtensions.HandleTransientHttpError().OrResult(msg => 
-            msg.StatusCode == HttpStatusCode.NotFound)
+    return HttpPolicyExtensions.HandleTransientHttpError().OrResult(message =>
+            message.StatusCode == HttpStatusCode.NotFound)
         .WaitAndRetryAsync(5,
             retryAttempt =>
             {
                 Debug.WriteLine($"Retry Count  :{retryAttempt}");
                 return TimeSpan.FromSeconds(10);
-            },onRetryAsync:OnRetryAsync);
+            }, onRetryAsync: OnRetryAsync);
 }
-
-
 
 Task OnRetryAsync(DelegateResult<HttpResponseMessage> arg1, TimeSpan arg2)
 {
@@ -57,13 +72,13 @@ Task OnRetryAsync(DelegateResult<HttpResponseMessage> arg1, TimeSpan arg2)
 }
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle  
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+// Configure the HTTP request pipeline.  
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
